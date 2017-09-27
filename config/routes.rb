11f34ef88274de36dc9c1326117428bebbca7500
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-27 20:04:21 +1000

    pages controller/views working & root set

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'demo#index'
+
+  get 'demo/index'
+
+  get 'pages/welcome'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
