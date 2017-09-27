88c866c2ae02e609f48b7a3a930d5348a8938b13
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-27 21:11:30 +1000

    working page links and params

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,17 @@ Rails.application.routes.draw do
 
   get 'demo/index'
 
+  get 'demo/hello'
+
+  get 'demo/other_hello'
+
+  get 'demo/lynda'
+
   get 'pages/welcome'
 
   get 'pages/contact'
 
   get 'pages/about'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
